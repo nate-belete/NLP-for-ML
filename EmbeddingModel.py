b3b79fd0ba6cca06dffa3632ebed7e3af420e462
natebelete1 <natebelete@gmail.com>
2022-11-24 08:52:02 -0800

    added create_target_context_pairs function

--- EmbeddingModel.py
+++ EmbeddingModel.py
@@ -1,39 +1,45 @@
 import tensorflow as tf
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
  
 # Skip-gram embedding model
 class EmbeddingModel(object):
    # Model Initialization
-   def __init__(self, vocab_size, embedding_dim):
+    def __init__(self, vocab_size, embedding_dim):
        self.vocab_size = vocab_size
        self.embedding_dim = embedding_dim
        self.tokenizer = tf.keras.preprocessing.text.Tokenizer(num_words=self.vocab_size)
  
    # Convert a list of text strings into word sequences
-   def tokenize_text_corpus(self, texts):
+    def tokenize_text_corpus(self, texts):
        # initialize Tokenizer with text
        self.tokenizer.fit_on_texts(texts)
        # convert text corpus into tokenized sequences
        sequences = self.tokenizer.texts_to_sequences(texts)
  
        return sequences
-       
-    # get target word and the left and right widnow size
-   def get_target_and_size(self, sequence, target_index, window_size):
+
+    # Convert a list of text strings into word sequences
+    def get_target_and_context(self, sequence, target_index, window_size):
         # target word
         target_word = sequence[target_index]
         # window size
         half_window_size = window_size // 2
-
-        return target_word, half_window_size
-
-    # Return the left (inclusive) and right (exclusive) indices of the window.
-   def  get_window_indices(self, sequence, target_index, half_window_size):
-        left_incl = max(0,target_index - half_window_size)
-        right_incl = min(len(sequence), target_index + half_window_size + 1)
-        return left_incl, right_incl
-
-    # Convert a list of text strings into word sequences
-   def get_target_and_context(self, sequence, target_index, window_size):
-        target_word, half_window_size = self.get_target_and_size(sequence, target_index, window_size)
-        left_incl, right_excl = self.get_window_indices(sequence, target_index, half_window_size)
+        # left window.
+        left_incl = max(0, target_index - half_window_size)
+        # right window.
+        right_excl = min(len(sequence), target_index + half_window_size + 1)
         return target_word, left_incl, right_excl
+
+    # Create (target, context) pairs for a given window size
+    def create_target_context_pairs(self, texts, window_size):
+        pairs = []
+        sequences = self.tokenize_text_corpus(texts)      
+        for sequence in sequences:
+            for i in range(len(sequence)):
+                target_word, left_incl, right_excl = self.get_target_and_context(
+                    sequence, i, window_size)
+                for j in range(left_incl, right_excl):
+                    if j != i:
+                        pairs.append((target_word, sequence[j]))
+        return pairs
